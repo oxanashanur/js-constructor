5de0a752b671a5faf88e0d99abfbd9e815861b0b
Oxana Bokievets <oxanaboki@gmail.com>
2024-04-05 16:29:35 +0900

    New project

---
+++ src/model.js
@@ -0,0 +1,49 @@
+import image from './assets/image.png'
+import {TextBlock, TitleBlock, ColumnsBlock, ImageBlock} from './classes/blocks'
+
+const text = `
+Any text
+`
+
+export const model = [
+  new TitleBlock('Title', {
+    tag: 'h2',
+    styles: {
+      background: 'linear-gradient(to right, #ff0099, #493240)',
+      color: 'green',
+      padding: '1.5rem',
+      'text-align': 'center'
+    }
+  }),
+  new ImageBlock(image, {
+    styles: {
+      padding: '2rem 0',
+      display: 'flex',
+      'justify-content': 'center'
+    },
+    imageStyles: {
+      width: '500px',
+      height: 'auto'
+    },
+    alt: 'Это картинка'
+  }),
+  new ColumnsBlock([
+    'Block1',
+    'Block2',
+    'Block3'
+  ], {
+    styles: {
+      background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+      padding: '2rem',
+      color: '#fff',
+      'font-weight': 'bold'
+    }
+  }),
+  new TextBlock(text, {
+    styles: {
+      background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+      padding: '1rem',
+      'font-weight': 'bold'
+    }
+  })
+]
